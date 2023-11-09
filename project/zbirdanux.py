e89ed0ec355f03415de3dd4756e35d92595d7afa
0legCH <olegpley2424@gmail.com>
2023-11-09 22:21:51 +0200

    main web

--- project/zbirdanux.py
+++ project/zbirdanux.py
@@ -5,7 +5,7 @@ import time
 import threading
 
 app = Flask(__name__)
-#
+#Початок логування
 logger = logging.getLogger('my_website')
 logger.setLevel(logging.DEBUG)
 
@@ -17,11 +17,31 @@ formatter = logging.Formatter('%(asctime)s - %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
         
-def logenter(username):
-    logger.debug(f'Enter the website: {username}')
+def logenter(username, mail):
+    logger.debug(f'Enter the website: "{username}" and email: "{mail}"')
 def logregistration(username, mail):
     logger.debug(f'User with name: "{username}" and email: "{mail}" has registered')
-#
+#Кінець логування
+#Список товарів
+products = {
+    1: {"name": "Mortal Kombat 11 Ultimate PS5", "description": '"Mortal Kombat 11" - це відеогра з жанру файтингу, випущена в 2019 році. Гра розвиває популярну серію "Mortal Kombat" і пропонує гравцям динамічні поєдинки між різними бійцями, кожен з яких має свої унікальні прийоми та смертоносні фінішери. "Mortal Kombat 11" славиться красивою графікою, кровопролитними битвами і широким списком персонажів, включаючи ветеранів серії та нових героїв. Гра також має захоплюючий сюжетний режим, який розкриває історію Мортал Комбату і його персонажів.', "price": 999, "image": "items/1.webp"},
+    2: {"name": "Товар 2", "description": "Властивості товару 2", "price": 20.00, "image": "items/2.webp"},
+    3: {"name": "Товар 1", "description": "Властивості товару 1", "price": 10.00, "image": "items/3.webp"},
+    4: {"name": "Товар 2", "description": "Властивості товару 2", "price": 20.00, "image": "items/4.webp"},
+    5: {"name": "Товар 1", "description": "Властивості товару 1", "price": 10.00, "image": "items/5.webp"},
+    6: {"name": "Товар 2", "description": "Властивості товару 2", "price": 20.00, "image": "items/6.webp"},
+    7: {"name": "Товар 1", "description": "Властивості товару 1", "price": 10.00, "image": "items/7.webp"},
+    8: {"name": "Товар 2", "description": "Властивості товару 2", "price": 20.00, "image": "items/8.webp"},
+    9: {"name": "Товар 1", "description": "Властивості товару 1", "price": 10.00, "image": "items/9.webp"},
+    10: {"name": "Товар 2", "description": "Властивості товару 2", "price": 20.00, "image": "items/10.webp"},
+    11: {"name": "Товар 1", "description": "Властивості товару 1", "price": 10.00, "image": "items/11.webp"},
+    12: {"name": "Товар 2", "description": "Властивості товару 2", "price": 20.00, "image": "items/12.webp"},
+    13: {"name": "Товар 1", "description": "Властивості товару 1", "price": 10.00, "image": "items/13.webp"},
+    14: {"name": "Товар 2", "description": "Властивості товару 2", "price": 20.00, "image": "items/14.webp"},
+    15: {"name": "Товар 1", "description": "Властивості товару 1", "price": 10.00, "image": "items/15.jpg"},
+    16: {"name": "Товар 2", "description": "Властивості товару 2", "price": 20.00, "image": "items/16.webp"},
+}
+#Кінець списку
 wrong_data_try = 0
 def reset_wrongdatatry():
     global wrongdatatry
@@ -37,6 +57,7 @@ def login_form():
     if request.method == "POST":
         username = str(request.form['username'])
         password = str(request.form['password'])
+        
         value = request.form['value']
         conn = sqlite3.connect('credentials.db')
         cursor = conn.cursor() 
@@ -47,7 +68,10 @@ def login_form():
             cursor.execute("SELECT * FROM credentials WHERE username=? AND password=?", (username, password))
             result = cursor.fetchone()
             if result:
-                logenter(username)
+                cursor.execute("SELECT mail FROM credentials WHERE username = ?;", (username,))
+                result = cursor.fetchone()
+                logenter(username, result)
+                
                 return 'main'
             else:
                 wrong_data_try += 1
@@ -117,6 +141,16 @@ def main():
 @app.route('/wrongdatalimit')
 def wrongdatalimit():
     return render_template('wrongdatalimit.html')
+
+
+@app.route('/product/<int:product_id>')
+def product_detail(product_id):
+    product = products.get(product_id)
+    if product is None:
+        return "Товар не знайдено"
+
+    return render_template('product_detail.html', product=product)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
